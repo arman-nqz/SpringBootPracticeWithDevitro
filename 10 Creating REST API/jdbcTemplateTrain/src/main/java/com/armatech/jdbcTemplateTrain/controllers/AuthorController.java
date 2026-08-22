2d0e284e0d19a59c10e8691d6c039c4e17b5f33c
Arman Naghizadeh <s240792@th-ab.de>
2026-08-22 16:27:11 +0200

    Commenting AuthorController so it is easier to understand

--- 10 Creating REST API/jdbcTemplateTrain/src/main/java/com/armatech/jdbcTemplateTrain/controllers/AuthorController.java
+++ 10 Creating REST API/jdbcTemplateTrain/src/main/java/com/armatech/jdbcTemplateTrain/controllers/AuthorController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+//The controller receives requests from outside world and return responses
+//receive the http req -> extract info from it -> calls service layer to perform business logic -> return result as http
 @RestController
 public class AuthorController {
 
@@ -20,10 +22,13 @@ public class AuthorController {
         this.authorMapper = authorMapper;
     }
 
-    @PostMapping(path = "/authors")
-    public AuthorDto createAuthor(@RequestBody AuthorDto author){
-        AuthorEntity authorEntity = authorMapper.mapFrom(author);
-        AuthorEntity savedAuthorEntity = authorService.createAuthor(authorEntity);
-        return authorMapper.mapTo(savedAuthorEntity);
+    //"Create" endpoint in CRUD
+    //create an API endpoint that accept author JSON convert it to an entity, save it, then returns the saved author as JSON
+    //@PostMapping tells run this when an http post request calls to authors
+    @PostMapping(path = "/authors")//this /authors is the endpoint
+    public AuthorDto createAuthor(@RequestBody AuthorDto author){//@RequestBody reads author send by client and turn it into AuthorDto
+        AuthorEntity authorEntity = authorMapper.mapFrom(author); //convert AuthorDto to authorEntity
+        AuthorEntity savedAuthorEntity = authorService.createAuthor(authorEntity); //calls service layer to save/create author
+        return authorMapper.mapTo(savedAuthorEntity);//convert saved authorEntity back into authorDto and it is then returned as the http req
     }
 }
--- 10 Creating REST API/jdbcTemplateTrain/src/main/java/com/armatech/jdbcTemplateTrain/repositories/AuthorRepository.java
+++ 10 Creating REST API/jdbcTemplateTrain/src/main/java/com/armatech/jdbcTemplateTrain/repositories/AuthorRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+//java component for communication with the database
+//job is data access
 @Repository
 public interface AuthorRepository extends CrudRepository<AuthorEntity, Long> {
     Iterable<AuthorEntity> ageLessThan(int age);
