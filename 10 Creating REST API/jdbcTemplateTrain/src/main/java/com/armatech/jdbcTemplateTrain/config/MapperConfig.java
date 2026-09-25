7b23c3e0564d7b4a21daf3ada184c0f1ecd85ca8
Arman Naghizadeh <s240792@th-ab.de>
2026-09-25 19:07:38 +0300

    fixing the nested author problem

--- 10 Creating REST API/jdbcTemplateTrain/src/main/java/com/armatech/jdbcTemplateTrain/config/MapperConfig.java
+++ 10 Creating REST API/jdbcTemplateTrain/src/main/java/com/armatech/jdbcTemplateTrain/config/MapperConfig.java
@@ -1,13 +1,18 @@
 package com.armatech.jdbcTemplateTrain.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration //for creating beans
 public class MapperConfig {
+
     @Bean //an obj that is managed by spring controller
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
     }
+
 }
